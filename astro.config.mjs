0e19b9fcf3e1bc04fa6a12cf0be02ea9396b2a92
Peacock <me@p3ac0ck.net>
2023-11-29 12:42:09 +0900

    :zap: Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-import remarkToc from 'remark-toc';
-import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+
+import remarkToc from "remark-toc";
+import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  site: "https://p3ac0ck.net",
+  integrations: [mdx(), sitemap(), tailwind()],
   markdown: {
     remarkPlugins: [[remarkToc, { heading: "目次" }]],
     rehypePlugins: [rehypeHeadingIds, rehypeAccessibleEmojis],
-  }
+  },
 });
