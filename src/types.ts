0a13770de67f891841e2f563522a084b16053195
Peacock <me@p3ac0ck.net>
2024-10-21 21:20:10 +0900

    Revert ":sparkles: Add og:image"
    This reverts commit a342c82ecf1a23f07f89d5413d9a19ef372557dd.

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,12 @@
 import type { MDXInstance } from "astro";
 
 export interface PostsFrontMatter {
-  slug: string;
   layout: string;
   title: string;
   lang: "en" | "ja";
   emoji: string;
   pubDate: string;
   description: string | null;
-  ogImage: string | null;
 }
 
 export interface Post extends MDXInstance<PostsFrontMatter> { }
