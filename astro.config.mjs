03950bf1c7576c55328db7e7c7b268eb7b56560c
Peacock <me@p3ac0ck.net>
2025-02-24 18:59:28 +0900

    :wheelchair: Add GA4 tag ID

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 
 import remarkToc from "remark-toc";
@@ -12,7 +13,7 @@ import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 // https://astro.build/config
 export default defineConfig({
   site: "https://p3ac0ck.net",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), partytown()],
   markdown: {
     remarkPlugins: [[remarkToc, { heading: "目次" }]],
     rehypePlugins: [rehypeHeadingIds, rehypeAccessibleEmojis],
